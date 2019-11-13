31da7d8cb89c08c1f85edb07a0fcdc05246598fd
Unknown <up201705616@fe.up.pt>
2019-11-13 09:38:02 +0000

    Patch probably done, need to parse controlPoints

--- Proj2/Patch.js
+++ Proj2/Patch.js
@@ -1,23 +1,30 @@
-class Patch extends CGFobject{
-    constructor(scene, npointsU, npointsV, npartsU, npartsV, controlPoints){
+class Patch extends CGFobject {
+    constructor(scene, npointsU, npointsV, npartsU, npartsV, controlPoints) {
         super(scene);
-        this.npointsU=npointsU;
-        this.npointsV=npointsV;
-        this.npartsU=npartsU;
-        this.npartsV=npartsV;
-        this.controlPoints=controlPoints;
+        this.npointsU = npointsU;
+        this.npointsV = npointsV;
+        this.npartsU = npartsU;
+        this.npartsV = npartsV;
+        this.controlPoints = controlPoints;
         this.init();
     }
 
-    init(){
-        this.controlVertexes=[];
+    init() {
+        this.controlVertexes = [];
 
-        for(var i=0; i<this.npointsU;i++){
+        for (var i = 0; i < this.npointsU; i++) {
             var aux = [];
-            for(var j=0;j<this.npointsV;j++){
-                aux.push([this.controlPoints[this.npointsV*1+j][0], [this.controlPoints[this.npointsV*1+j][1]], [this.controlPoints[this.npointsV*1+j][2], [this.controlPoints[this.npointsV*1+j][3]);
+            for (var j = 0; j < this.npointsV; j++) {
+                aux.push([this.controlPoints[this.npointsV * i + j][0], this.controlPoints[this.npointsV * i + j][1], this.controlPoints[this.npointsV * i + j][2], 1]);
             }
             this.controlVertexes.push(aux);
         }
+
+        this.nurbsSurface = new CGFnurbsSurface(this.npointsU-1, this.npointsV-1, this.controlVertexes);
+        this.nurbsObject = new CGFnurbsObject(this.scene, this.npartsU, this.npartsV, this.nurbsSurface);
+    }
+
+    display() {
+        this.nurbsObject.display();
     }
 }
